a2c092e841dc0a4c440c7953fdf45af39a5b0d27
CesarQuint <pedrocesarquintero001@gmail.com>
2023-02-13 11:23:53 -0600

    Some

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import AuthLayout from "./layout/AuthLayout"
+import ConfirmarCuenta from "./paginas/ConfirmarCuenta"
+import Login from "./paginas/Login"
+import NuevoPassword from "./paginas/NuevoPassword"
+import OlvidePassword from "./paginas/OlvidePassword"
+import Registrar from "./paginas/Registrar"
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout/>}>
+          <Route index element={<Login/>}/>
+          <Route path="registrar" element={<Registrar/>}/>
+          <Route path="olvide-password" element={<OlvidePassword/>}/>
+          <Route path="olvide-password/:token" element={<NuevoPassword/>}/>
+          <Route path="confimar/:id" element={<ConfirmarCuenta/>}/>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
